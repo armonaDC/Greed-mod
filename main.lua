699816e788242fcd09cf1e070ee32f62f1b32b05
armonaDC <armonaDC@gmail.com>
2024-01-03 12:55:55 -0800

    added RoomDescriptor objects

--- main.lua
+++ main.lua
@@ -2,12 +2,16 @@ local mod = RegisterMod("Greed mod", 1)
 
 local game = Game() --game object so we can call functions from the game class
 local rng = RNG() --rng object used for QueryRoomTypeIndex
-local startRoomIndex = game:GetLevel():GetCurrentRoomIndex()
+local startRoomIndex = game:GetLevel():QueryRoomTypeIndex(RoomType.ROOM_DEFAULT, false, rng, false) --room type found from in game console
 local treasureRoomIndex = 85 --value found from in game console
 local shopRoomIndex = game:GetLevel():QueryRoomTypeIndex(RoomType.ROOM_SHOP, false, rng, false)
 local curseRoomIndex = game:GetLevel():QueryRoomTypeIndex(RoomType.ROOM_CURSE, false, rng, false)
 local bossItemRoomIndex = 98 --value found from in game console
 
+TreasureRoomDesc = game:GetLevel():GetRoomByIdx(treasureRoomIndex, -1) --RoomDescriptor object
+ShopRoomDesc = game:GetLevel():GetRoomByIdx(shopRoomIndex, -1)
+CurseRoomDesc = game:GetLevel():GetRoomByIdx(curseRoomIndex, -1)
+BossItemRoomDesc = game:GetLevel():GetRoomByIdx(bossItemRoomIndex, -1)
 
 function mod:checkItems()
     local player = game:GetPlayer(0)
