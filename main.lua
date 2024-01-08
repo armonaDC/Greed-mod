c18f51f48a7196ccaa267aee3065715782a2319c
armonaDC <armonaDC@gmail.com>
2024-01-08 11:30:29 -0800

    deleted Game() object

--- main.lua
+++ main.lua
@@ -1,26 +1,25 @@
 local mod = RegisterMod("Greed mod", 1)
 
-local game = Game() --game object so we can call functions from the game class
 local rng = RNG() --rng object used for QueryRoomTypeIndex
-local startRoomIndex = game:GetLevel():QueryRoomTypeIndex(RoomType.ROOM_DEFAULT, false, rng, false) --room type found from in game console
-local treasureRoomIndex = 85 --value found from in game console
-local shopRoomIndex = game:GetLevel():QueryRoomTypeIndex(RoomType.ROOM_SHOP, false, rng, false)
-local curseRoomIndex = game:GetLevel():QueryRoomTypeIndex(RoomType.ROOM_CURSE, false, rng, false)
-local bossItemRoomIndex = 98 --value found from in game console
+local startRoomIndex = Game():GetLevel():QueryRoomTypeIndex(RoomType.ROOM_DEFAULT, false, rng, false) --room type found from in Game() console
+local treasureRoomIndex = 85 --value found from in Game() console
+local shopRoomIndex = Game():GetLevel():QueryRoomTypeIndex(RoomType.ROOM_SHOP, false, rng, false)
+local curseRoomIndex = Game():GetLevel():QueryRoomTypeIndex(RoomType.ROOM_CURSE, false, rng, false)
+local bossItemRoomIndex = 98 --value found from in Game() console
 -- BossRoomVisited = false
 -- TreasureRoomVisited = false
 
 function mod:Teleport(roomIndex)
-    game:StartRoomTransition(roomIndex, Direction.NO_DIRECTION, RoomTransitionAnim.PORTAL_TELEPORT, game:GetPlayer(0), -1) --teleports isaac to room index
+    Game():StartRoomTransition(roomIndex, Direction.DOWN, RoomTransitionAnim.PORTAL_TELEPORT, Game():GetPlayer(0), -1) --teleports isaac to room index
 
-    -- if game:GetLevel():GetRoomByIdx(98, -1) then BossRoomVisited = true end
-    -- if game:GetLevel():GetRoomByIdx(85, -1) then TreasureRoomVisited = true end
+    -- if Game():GetLevel():GetRoomByIdx(98, -1) then BossRoomVisited = true end
+    -- if Game():GetLevel():GetRoomByIdx(85, -1) then TreasureRoomVisited = true end
 end
 
 function mod:FindAndPrintItems()
     local itemTable = Isaac.FindByType(EntityType.ENTITY_PICKUP, PickupVariant.PICKUP_COLLECTIBLE, -1, false, false)
     --finds collectibles in a room and puts them in an array or "table" in lua
-    mod:PrintItems(itemTable) --prints collectibleID to game console
+    mod:PrintItems(itemTable) --prints collectibleID to Game() console
 end
 
 function mod:StartMod()
@@ -43,6 +42,7 @@ function mod:StartMod()
 end
 
 function mod:PrintItems(table)
+    print("PrintItems started")
     for i=1,4 do --lua starts counting from 1, checking up to 4 pedestal items but idk if this is even possible
         if table[i] ~= nil then --check to make sure something exists at that index
             print(table[i].Subtype)
@@ -53,7 +53,7 @@ function mod:PrintItems(table)
 end
 
 function mod:ConditionToStartMod()
-    if ((game:GetLevel():GetStage() == 1) and (game:IsGreedMode() == true)) then
+    if ((Game():GetLevel():GetStage() == 1) and (Game():IsGreedMode() == true)) then
         mod:StartMod()
     end
 end
